5f1bd62222538c45857be51a7de8d60b4f31228a
Susan Lee <susanlee@Susans-MacBook-Air.local>
2024-11-12 11:10:33 +0700

    fix load

--- rock-paper-scissors/main.cpp
+++ rock-paper-scissors/main.cpp
@@ -5,20 +5,25 @@ using namespace std;
 
 int a;
 int acak = rand() % 3;
+int x = 10;
 
 void loading(string a){
     for (int i = 0; i < 10; i++){
         usleep(500000);
+        string o (x, ' ');
         string j(i, '[');
         string k(i, ']');
-        cout << j << a << k << endl;
+        cout << o << j << a << k << endl;
         cout << "\033[2J\033[1;1H";
+        x = x-1;
     }
 }
 
-void test(){
-
+void wrong(){
+    cout << "Wrong Character!";
+    menu();
 }
+
 void rps(){
     loading("loading");
 }
@@ -29,7 +34,7 @@ void menu(){
     cout << "2. Exit\n";
     cout << "=";
     cin >> a ;
-    a == 1 ? rps() : a == 2 ? test() :exit(0);
+    a == 1 ? rps() : a == 2 ? exit(0) : wrong();
 }
 
 int main(){
